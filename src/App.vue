0dfe1996f307442af407b0664dfee627ef666636
leandro-hd <leandrohg2003@gmail.com>
2021-12-02 22:54:32 -0300

    chore: Iniciando projeto

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>{{ titulo }}</h1>
+
+    <ul>
+      <li v-for="foto of fotos">
+        <img :src="foto.url" :alt="foto.titulo">
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+
+  data() {
+    return {
+      titulo: 'Alurapic',
+      fotos: [
+        {
+          url: 'https://conteudo.imguol.com.br/c/entretenimento/54/2020/04/28/cachorro-pug-1588098472110_v2_900x506.jpg.webp',
+          titulo: 'Cachorro'
+        },
+        {
+          url: 'https://conteudo.imguol.com.br/c/entretenimento/54/2020/04/28/cachorro-pug-1588098472110_v2_900x506.jpg.webp',
+          titulo: 'Cachorrão'
+        }
+      ]
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
