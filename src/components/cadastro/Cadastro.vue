b1b5f122c789f7170bbd9a087ddf52a970a67f6f
leandro-hd <leandrohg2003@gmail.com>
2022-01-03 15:12:52 -0300

    chore: P2-05 - Validando formulários

--- src/components/cadastro/Cadastro.vue
+++ src/components/cadastro/Cadastro.vue
@@ -10,12 +10,14 @@
     <form @submit.prevent="grava()">
       <div class="controle">
         <label for="titulo">TÍTULO</label>
-        <input id="titulo" autocomplete="off" v-model.lazy="foto.titulo">
+        <input data-vv-as="título" name="titulo" v-validate data-vv-rules="required|min:3|max:30" id="titulo" autocomplete="off" v-model="foto.titulo">
+        <span class="erro" v-show="errors.has('titulo')">{{ errors.first('titulo') }}</span>
       </div>
 
       <div class="controle">
         <label for="url">URL</label>
-        <input v-model.lazy="foto.url" id="url" autocomplete="off">
+        <input name="url" v-validate data-vv-rules="required" v-model="foto.url" id="url" autocomplete="off">
+        <span class="erro" v-show="errors.has('url')">{{ errors.first('url') }}</span>
         <imagem-responsiva :url="foto.url" :titulo="foto.titulo"/>
       </div>
 
@@ -61,14 +63,19 @@ export default {
   methods: {
 
     grava() {
-      this.service
-        .cadastra(this.foto)
-        .then(() => {
-          if (this.id) this.$router.push({ name: 'home' });
-          this.foto = new Foto()
-        }, err => console.log(err));
+      this.$validator
+        .validateAll()
+        .then(success => {
+          if (success) {
+            this.service
+              .cadastra(this.foto)
+              .then(() => {
+                if (this.id) this.$router.push({ name: 'home' });
+                this.foto = new Foto()
+              }, err => console.log(err));
+          }
+        });
     }
-
   },
 
   created () {
@@ -108,4 +115,7 @@ export default {
     text-align: center;
   }
 
+  .erro {
+    color: red;
+  }
 </style>
